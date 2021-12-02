a4e41c3912eb4e4cd505cff19b79d3fc2824fa7c
Eugene 'marengohue' Rebedailo <mokonamodokidesu@gmail.com>
2021-12-02 21:44:38 +0100

    Generalize solution for p1 and p2

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -1,35 +1,54 @@
 use std::io::{BufRead, stdin};
 
+pub enum SubOrderMode {
+    Polar,
+    Eucledian
+}
+
 pub struct Submarine {
+    mode: SubOrderMode,
     pos: (i32, i32),
     aim: i32,
 }
 
 impl Submarine {
-    pub fn new() -> Self {
+    pub fn new(mode: SubOrderMode) -> Self {
         Submarine {
             pos: (0, 0),
-            aim: 0
+            aim: 0,
+            mode
         }
     }
     
     pub fn forward(&mut self, by: i32) {
-        let (x, y) = self.pos;
-        self.pos = (
-            x + by,
-            y + self.aim * by
-        )
+        match self.mode {
+            SubOrderMode::Polar => {
+                self.pos.0 += by;
+                self.pos.1 += self.aim * by;
+            },
+            SubOrderMode::Eucledian => {
+                self.pos.0 += by;
+            }
+        }
     }
 
     pub fn steer(&mut self, by: i32) {
-        self.aim += by;
+        match self.mode {
+            SubOrderMode::Polar => {
+                self.aim += by;
+            },
+            SubOrderMode::Eucledian => {
+                self.pos.1 += by;
+            }
+        }
+
     }
 }
 
 fn main() {
     let stdin = stdin();
     let handle = stdin.lock();
-    let mut sub = Submarine::new();
+    let mut sub = Submarine::new(SubOrderMode::Eucledian);
 
     for maybe_line in handle.lines() {
         let line = maybe_line.unwrap();
